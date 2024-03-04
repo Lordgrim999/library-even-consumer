3865372f6ea369bdba85b9ea731b0452e49e79b6
Lordgrim999 <srivastavaak47rishi@gmail.com>
2024-03-04 08:53:30 +0530

    adding all files

---
+++ src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
@@ -0,0 +1,23 @@
+package com.learnkafka.config;
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.boot.ssl.SslBundles;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.ContainerCustomizer;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+
+import java.util.Objects;
+
+@Configuration
+public class LibraryEventsConsumerConfig {
+
+
+
+
+}
